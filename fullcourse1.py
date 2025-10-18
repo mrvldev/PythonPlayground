0cad400361581956d40ee7dcbfd4520496b709fe
mrvldev <mikerawvonl@gmail.com>
2025-10-18 09:17:02 +0200

    startet a tutorial on youtube and sortet the old codes into a folder "old" to have a clean working directory for the tutorial  codes.

---
+++ fullcourse1.py
@@ -0,0 +1,35 @@
+text   = 'Text in python is called string'
+text_2 = "It has to be inside single or double qoutes"
+text_3 = 'We can use "double quotes" inside  single qoutes'
+text_4 = "And ‘vice versa‘ " # This is a string
+
+
+# But we can not write text in pthon without quotes
+# It will break syntax rule
+
+print(text)
+print(text_2)
+print(text_3)
+print(text_4)
+
+# Do not use as variable or function
+# print
+# type
+# sum
+# filter
+# id
+
+# not
+# in
+# break
+# continue
+# for
+# if
+# elif
+# else
+
+# str
+# float
+# list
+# bool
+# int
